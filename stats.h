98aad076bccba1ccb102b5cb26e786c2a0764def
sayooj555 <105042485+sayooj555@users.noreply.github.com>
2022-05-08 23:47:06 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -1,5 +1,25 @@
+#include <iostream>
 #include <vector>
+using namespace std;{
+ Stats ComputeStatistics(const std::vector<___>& );
+int main() {
+  vector<int> num {1, 2, 3, 4, 5};
 
-namespace Statistics {
-    Stats ComputeStatistics(const std::vector<___>& );
+  cout << "Initial Vector: ";
+
+  for (const int& i : num) {
+    cout << i << "  ";
+  }
+  
+  
+  num.push_back(6);
+  num.push_back(7);
+
+  cout << "\nUpdated Vector: ";
+
+  for (const int& i : num) {
+    cout << i << "  ";
+  }
+
+  return 0;
 }
